fe05ff68370e9bbc5f6e1e1b561d9271c3d2cbd7
DCasalNovo <52653136+DCasalNovo@users.noreply.github.com>
2022-05-14 02:46:50 +0100

    primeira parte do parser feito

--- TP2/lexer.py
+++ TP2/lexer.py
@@ -1,50 +1,56 @@
 import ply.lex as lex
 
-tokens = ["ID", "LEX", "YACC", "TOKENS", "IGNORE", "LITERALS", "STRING", "REGEX", "P", "RETURN", "ERROR"] 
-literals = ['=', '"', '[', ']', ',', '(', ')', '{', '}', ':', '%', '.']
+tokens = ["ID", "LEX", "YACC", "TOKENS", "IGNORE", "LITERALS", "STRING", "REGEX", "NUM", "P"] 
+literals = ['=', '"', '[', ']', ',', '{', '}', ':', '%', '.', '(', ')']
 
 def t_LEX(t):
     r'LEX'
+    print(t)
     return t
 
 def t_YACC(t):
     r'YACC'
+    print(t)
     return t
 
 def t_TOKENS(t):
     r'tokens'
+    print(t)
     return t
 
 def t_IGNORE(t):
     r'ignore'
+    print(t)
     return t
 
 def t_LITERALS(t):
     r'literals'
-    return t
-
-def t_RETURN(t):
-    r'return'
-    return t
-
-def t_ERROR(t):
-    r'error'
+    print(t)
     return t
 
 def t_P(t):
     r'\''
+    print(t)
     return t
 
 def t_STRING(t):
     r'f?\"[^"]*\"'
+    print(t)
     return t
 
 def t_REGEX(t):
-    r'r\'[^ ]+\''
+    r'r\'[^\']+\''
+    print(t)
     return t
 
 def t_ID(t):
-    r'[a-zA-Z_]\w*'
+    r'[A-Za-z_]\w*'
+    print(t)
+    return t
+
+def t_NUM(t):
+    r'\d+'
+    print(t)
     return t
 
 t_ignore = " \n\t"
--- TP2/parser.py
+++ TP2/parser.py
@@ -2,7 +2,7 @@ import ply.yacc as yacc
 from lexer import tokens, literals 
 
 def p_Program(p):
-    "Program : '%' '%' LEX Lex '%' '%' YACC Yacc"
+    "Program : '%' '%' LEX Lex '%' '%' YACC Yacc '%' '%' Main"
 
 def p_Lex(p):
     "Lex : Dec Def"
@@ -34,23 +34,62 @@ def p_Tokens2(p):
 def p_String(p):
     "String : STRING"
 
-def p_Def2_return(p):
-    "Def2 : REGEX RETURN '(' P ID P ','  ')'"
-
-def p_Def2_error(p):
-    "Def2 : '.' ERROR '(' ')'"
-
 def p_Def_list(p):
     "Def : Def Def2"
 
 def p_Def_single(p):
     "Def : Def2"
 
+def p_Def2_newDef(p):
+    "Def2 : REGEX ID '(' P ID P ',' Func ')'"
+
+def p_Def2_error(p):
+    "Def2 : '.' ID '(' STRING ',' Func ')'"
+
+
+def p_Func_id(p):
+    "Func : ID Exp"
+
+def p_Func_num(p):
+    "Func : NUM"
+
+def p_Exp_list(p):
+    "Exp : Exp Exp2"
+
+def p_Exp_empty(p):
+    "Exp : "
+
+def p_Exp2_id(p):
+    "Exp2 : '.' ID"
+
+def p_Exp2_par(p):
+    "Exp2 : '(' Args ')'"
+
+def p_Args_list(p):
+    "Args : Args ',' Func"
+
+def p_Args_single(p):
+    "Args : Func"
+
+def p_Args_empty(p):
+    "Args : "
+
+
+
 
 
 
 def p_Yacc(p):
     "Yacc : "
+
+def p_Main_list(p):
+    "Main : Main Main2"
+
+def p_Main_single(p):
+    "Main : Main2"
+
+def p_Main2(p):
+    "Main2 : "
 
 
 
